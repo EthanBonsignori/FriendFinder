9c858954d0b3f62472fcba1f5f4d04f3aff7757a
EthanB <ebonsignori@gmail.com>
2019-05-19 18:54:40 -0400

    friends added to friends.js

---
+++ app/data/friend.js
@@ -0,0 +1,29 @@
+const friends = [
+  {
+    name: 'Arató András',
+    photo: 'https://ih0.redbubble.net/image.345293314.1947/mp,550x550,matte,ffffff,t.3.jpg',
+    scores: ['1', '5', '2', '3', '2', '1', '5', '5', '4', '4']
+  },
+  {
+    name: 'Judith Jool',
+    photo: 'https://bobandsuewilliams.com/images/weird-stock-9.jpg',
+    scores: ['2', '3', '1', '3', '5', '5', '3', '1', '4', '3']
+  },
+  {
+    name: 'Christoph Christopherson',
+    photo: 'https://i.imgur.com/RhiEi1I.jpg',
+    scores: ['3', '3', '3', '4', '1', '2', '2', '4', '5', '1']
+  },
+  {
+    name: 'Lob "The Lobster" Johnson',
+    photo: 'https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX3446927.jpg',
+    scores: ['1', '4', '2', '4', '1', '4', '5', '2', '5', '3']
+  },
+  {
+    name: 'Andy',
+    photo: 'https://i.pinimg.com/236x/4f/bb/48/4fbb4825cba6b7ce8abf86e9722c87cb--photos-google-photographs.jpg',
+    scores: ['3', '1', '1', '1', '5', '5', '5', '4', '2', '1']
+  },
+]
+
+module.exports = friends
