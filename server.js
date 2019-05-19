c055a0c64005adfc66dec75f811a95a5c0c0f8a2
EthanB <ebonsignori@gmail.com>
2019-05-19 18:30:56 -0400

    server.js connecting thru express

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 const path = require('path')
 const express = require('express')
+
+const app = express()
+const port = process.env.port || 3000
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+require('./app/routing/apiRoutes')(app)
+require('./app/routing/htmlRoutes')(app)
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
